ead3c6bddecd28345c643ee2821f408c898ad244
LucasAlves38 <lucasalvesrealms@gmail.com>
2026-09-24 19:23:33 -0300

    projeto finalizado

--- index.js
+++ index.js
@@ -17,21 +17,21 @@ const local3 = "casa"
 const dias3 = 1
 
 // urgencia1 vai ser definida depois, por isso uso let e nao const
-let urgencia1;
+let urgencia1
 if (dias1 <= 3) {
   urgencia1 = "URGENTE"
 } else {
   urgencia1 = "normal"
 }
 
-let urgencia2;
+let urgencia2
 if (dias2 <= 3) {
   urgencia2 = "URGENTE"
 } else {
   urgencia2 = "normal"
 }
 
-let urgencia3;
+let urgencia3
 if (dias3 <= 3) {
   urgencia3 = "URGENTE"
 } else {
@@ -40,10 +40,9 @@ if (dias3 <= 3) {
 
 // mostra o titulo do programa
 console.log("--- Não esquecer ---")
-console.log("")
+
 
 // mostra as informacoes de cada atividade juntas numa linha
 console.log(atividade1 + " | " + local1 + " | " + dias1 + " dias | " + urgencia1)
 console.log(atividade2 + " | " + local2 + " | " + dias2 + " dias | " + urgencia2)
 console.log(atividade3 + " | " + local3 + " | " + dias3 + " dias | " + urgencia3)
-
